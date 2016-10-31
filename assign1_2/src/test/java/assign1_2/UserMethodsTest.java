9b0a503dbf81f938ad7de4e57e3dc0f60b6d5184
Roxana Roman <Roxana Roman>
2016-10-31 14:11:05 +0200

    changed junit 3 to junit 4 removed jsp servlet dependency add tomcat 7 mvn plugin
    Run as:
    Step 1: mvn clean install
    Step 2: mvn tomcat7:run

--- assign1_2/src/test/java/assign1_2/UserMethodsTest.java
+++ assign1_2/src/test/java/assign1_2/UserMethodsTest.java
@@ -1,48 +1,54 @@
-// package assign1_2;
-//
-// import org.junit.Before;
-// import org.junit.Test;
-//
-// import com.bogdan.assign2.model.User;
-// import com.bogdan.assign2.model.UserRole;
-// import com.bogdan.assign2.repository.BaseRepository;
-// import com.bogdan.assign2.service.LoginService;
-//
-// public class UserMethodsTest {
-// private BaseRepository bs = new BaseRepository();
-// private User user = new User();
-//
-// private LoginService ls = new LoginService();
-//
-// @Before
-// public void initializeUser() {
-// this.user.setId(new Long(0));
-// this.user.setFirstName("Bogdan");
-// this.user.setLastName("Roman");
-// this.user.setUsername("romanb");
-// this.user.setPassword("1212");
-// this.user.setUserRole(UserRole.ADMIN);
-//
-// }
-//
-// @Test
-// public void testSave() {
-// User u = new User();
-//
-// u.setUsername("romanbbb");
-// u.setPassword("1212");
-// u = ls.login(u);
-// if (u.getUserRole().name().equals("CLIENT")) {
-// System.out.println(u.getUserRole().name());
-// }
-// }
-//
-// // @Test
-// // public void testFindByUsername() {
-// //
-// // LoginService ls = new LoginService();
-// //
-// // assertEquals("Find by username: ", this.user.getUsername(),
-// // ls.getUserByUsername(this.user.getUsername()).getUsername());
-// // }
-// }
+package assign1_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bogdan.assign2.model.User;
+import com.bogdan.assign2.model.UserRole;
+import com.bogdan.assign2.repository.BaseRepository;
+import com.bogdan.assign2.service.LoginService;
+
+public class UserMethodsTest {
+    
+    private BaseRepository bs = new BaseRepository();
+    
+    private User user = new User();
+    
+    private LoginService ls = new LoginService();
+    
+    //TODO Should use @BeforeClass to create a test user and 
+    // @AfterClass to remove the user at the end of the test
+    // @Before = code is executed before each test
+    //@BeforeClass runs once at the begining of the test class
+    //same with @after, @afterclass
+    @Before
+    public void initializeUser() {
+        this.user.setId(new Long(0));
+        this.user.setFirstName("Bogdan");
+        this.user.setLastName("Roman");
+        this.user.setUsername("romanb");
+        this.user.setPassword("1212");
+        this.user.setUserRole(UserRole.ADMIN);
+       // bs.save(user);
+    }
+    
+    //this test is not a test
+    @Test
+    public void testSave() {
+        User u = new User();
+        u.setUsername("romanbbb");
+        u.setPassword("1212");
+        u.setUserRole(UserRole.CLIENT);
+        u = ls.login(u);
+    }
+    
+    @Test
+    public void testFindByUsername() {
+        
+        LoginService ls = new LoginService();
+        assertEquals("Find by username: ", this.user.getUsername(), ls.getUserByUsername(this.user.getUsername())
+                .getUsername());
+    }
+}
